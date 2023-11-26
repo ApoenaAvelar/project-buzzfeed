330976ec2c03c3af39dd782898856967e5fad959
Apoena Avelar <apoenajorge@gmail.com>
2023-11-26 18:28:40 -0300

    Quizz logic partially implemented

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json';
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
-export class QuizzComponent {
+export class QuizzComponent implements OnInit {
+
+    title: string = ""
+
+    questions: any
+    pickedQuestion: any
+
+    answers: string[] = []
+    pickedAnswer: string = ""
+
+    questionIndex: number = 0
+    questionMaxIndex: number = 0
+
+    finished: boolean = false
+
+    constructor() { }
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.pickedQuestion = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length;
+
+
+
+    }
+
+  }
+
+  onOptionClicked(value: string) {
+    this.answers.push(value)
+    console.log(this.answers)
+
+  }
+
+  async nextStep(){
+    this.questionIndex++
+    if (this.questionIndex < this.questionMaxIndex) {
+        this.pickedQuestion = this.questions[this.questionIndex]
+    } else {
+        this.finished = true
+    }
+  }
+
 
 }
